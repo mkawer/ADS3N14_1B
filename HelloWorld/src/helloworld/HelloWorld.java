99fadd0b510814757799c84bc8c2db97d426269b
mkawer <mkawer@gmail.com>
2014-04-22 20:17:45 -0300

    Alteração da mensagem.

--- HelloWorld/src/helloworld/HelloWorld.java
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -8,7 +8,7 @@ package helloworld;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello, you");
     }
     
 }
