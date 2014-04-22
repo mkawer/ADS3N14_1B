783ca34783e5c6f4d68e987f697ddc17ecd8fbc3
mkawer <mkawer@gmail.com>
2014-04-22 20:15:55 -0300

    Meu primeiro commit

---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package helloworld;
+
+/**
+ *
+ * @author Marcello Rebelo
+ * Classe que escreve a mensagem "Hello World" na tela.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
